5cf84319c81df240f10af8a336e7700d5bde1d46
ItsAndrewC! <andew73885@gmail.com>
2024-12-21 12:01:19 -0500

    install pug for html rendering

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express')
-const path = require('path');
 const app = express()
 const port = 500
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/routes')
+app.set('view options', { layout: false });
 
 app.listen(port, (err) => {
     console.log('Server has started on: http://localhost:' + port);
 })
 
 app.get('/', (req, res) => {
-    console.log(req.method);
-    res.render('index.html')
-    res.end()
+    res.render('index.jade')
 })
